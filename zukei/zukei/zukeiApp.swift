4a108f0d98ad7820c5abc3eb44901117f97bb967
市川マサル <>
2021-04-28 22:54:05 +0900

    図形プッシュ

---
+++ zukei/zukei/zukeiApp.swift
@@ -0,0 +1,17 @@
+//
+//  zukeiApp.swift
+//  zukei
+//
+//  Created by 市川マサル on 2021/04/28.
+//
+
+import SwiftUI
+
+@main
+struct zukeiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
