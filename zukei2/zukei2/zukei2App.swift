af3c11461b7fc5fc5389fff26a6f48a18c76a900
市川マサル <>
2021-04-29 00:03:27 +0900

    図形のポジション

---
+++ zukei2/zukei2/zukei2App.swift
@@ -0,0 +1,17 @@
+//
+//  zukei2App.swift
+//  zukei2
+//
+//  Created by 市川マサル on 2021/04/29.
+//
+
+import SwiftUI
+
+@main
+struct zukei2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
