3dc2b81fb04e7362359dd75d0a33d63367d8847f
市川マサル <>
2021-04-29 16:41:31 +0900

    配列を連結したものソートしたもの

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,5 +1,25 @@
 import UIKit
-var colors = ["green","red","blue","pink"]
-//書き換える
-colors[1...2] = ["赤","青","黄"]
-print(colors)
+//初期化した配列String型
+var personas = [String]()
+//要素を加える
+personas.append("マーラ")
+personas.append("ルシファー")
+personas.append("ラクシャーサ")
+//要素を複数いっぺんに加える
+personas.append(contentsOf: ["ヨシツネ","マサカド","オオクニヌシ"])
+print(personas)
+
+//初期化済みの配列を宣言
+var data = Array<Double>()
+//数値が入った配列
+let data1 = [3.6,5.7,1.2]
+let data2 = [2.2,9.5,3.3]
+
+//配列を連結する
+data += data1
+data += data2
+
+//配列をソートする。
+data.sort()
+print(data)
+
