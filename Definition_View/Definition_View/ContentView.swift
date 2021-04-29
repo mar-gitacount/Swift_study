a3d1df33da1d17a5ffd54a3480961b43332a146f
市川マサル <>
2021-04-29 12:41:51 +0900

    ビューを利用してオブジェクトを利用する。

--- Definition_View/Definition_View/ContentView.swift
+++ Definition_View/Definition_View/ContentView.swift
@@ -9,8 +9,37 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        HStack {
+            //Flowerビューの呼び出し
+            Flower()
+                .frame(height: 80)
+            //テキストの呼び出し
+            Hello()
+                .font(.system(size: 40))
+                .padding()
+            //Flowerビューの呼び出し
+            Flower()
+                .frame(height:80)
+        }
+    }
+}
+//Helloビューの定義
+struct Hello: View{
+    var body: some View{
+        Text("HelloWorld")
+            .foregroundColor(.pink)
+            .fontWeight(.thin)
+            .multilineTextAlignment(.center)
+    }
+}
+
+//Flowerビューの定義
+struct Flower: View{
+    var body: some View{
+        Image("flower")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            
     }
 }
 
