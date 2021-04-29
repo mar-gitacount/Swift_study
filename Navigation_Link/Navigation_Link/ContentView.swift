6862c6a4a7d5c13080ea4caa5905b1bcd909eafa
市川マサル <>
2021-04-29 13:05:02 +0900

    リストに画像を挿入する

--- Navigation_Link/Navigation_Link/ContentView.swift
+++ Navigation_Link/Navigation_Link/ContentView.swift
@@ -11,9 +11,23 @@ struct ContentView: View {
     var body: some View {
         List{
             Text("Content1")
+            Photo1().frame(height:150)
             Text("Content2")
             Text("Content3")
             Text("Content4")
+        }
+    }
+}
+
+//写真１
+struct Photo1: View {
+    var body: some View{
+        HStack{
+            Image("bus")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+            Text("えぼし号")
+                .padding(.horizontal)
         }
     }
 }
