693dc29974402b9087e5fe5c093217add852c79d
市川マサル <>
2021-04-29 11:04:55 +0900

    画像を円形にクリッピングする。

--- image_rotate/image_rotate/ContentView.swift
+++ image_rotate/image_rotate/ContentView.swift
@@ -9,8 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        //画像を表示する。
+        Image("km169459347804110212339")
+            //リサイズする
+            .resizable()
+           //領域を埋めるように表示する。
+            .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
+            .frame(width:300,height: 300)
+            //画像を円形にする。
+            .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
     }
 }
 
