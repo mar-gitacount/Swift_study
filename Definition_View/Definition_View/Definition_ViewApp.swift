dd81595a936b5a8a86ea9b27ceeaadd90d446564
市川マサル <>
2021-04-29 12:18:54 +0900

    ビュー定義

---
+++ Definition_View/Definition_View/Definition_ViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  Definition_ViewApp.swift
+//  Definition_View
+//
+//  Created by 市川マサル on 2021/04/29.
+//
+
+import SwiftUI
+
+@main
+struct Definition_ViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
