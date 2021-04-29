b9b0389af351e0b2cd94fd51d1e62aa85b4708ea
市川マサル <>
2021-04-29 13:59:50 +0900

    Embed in Listを使うことで繰り返し文を表示することができる。

---
+++ array_list/array_list/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  array_list
+//
+//  Created by 市川マサル on 2021/04/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        List(/*@START_MENU_TOKEN@*/0 ..< 5/*@END_MENU_TOKEN@*/) { item in
+            Text("Hello, world!")
+                .padding()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ array_list/array_list/array_listApp.swift
@@ -0,0 +1,17 @@
+//
+//  array_listApp.swift
+//  array_list
+//
+//  Created by 市川マサル on 2021/04/29.
+//
+
+import SwiftUI
+
+@main
+struct array_listApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
