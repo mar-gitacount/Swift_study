3158c6609a5ceee5c4bcbe6303a1610cbaaa9b91
市川マサル <>
2021-04-29 16:58:06 +0900

    配列のスライスについて

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,25 +1,9 @@
 import UIKit
-//初期化した配列String型
-var personas = [String]()
-//要素を加える
-personas.append("マーラ")
-personas.append("ルシファー")
-personas.append("ラクシャーサ")
-//要素を複数いっぺんに加える
-personas.append(contentsOf: ["ヨシツネ","マサカド","オオクニヌシ"])
-print(personas)
-
-//初期化済みの配列を宣言
-var data = Array<Double>()
-//数値が入った配列
-let data1 = [3.6,5.7,1.2]
-let data2 = [2.2,9.5,3.3]
-
-//配列を連結する
-data += data1
-data += data2
-
-//配列をソートする。
-data.sort()
-print(data)
-
+//配列を定義
+let personas = ["ルシファー","ミカエル","ガブリエル","ヨシツネ","マサカド","オオクニヌシ"]
+//countを使って半分に分ける
+let harf = personas.count / 2
+let personas1 = personas[..<harf]
+let personas2 = personas[harf...]
+print(personas1)
+print(personas2)
