d43c9091941592dceb112eda8214691a5604bb13
市川マサル <>
2021-04-29 17:20:41 +0900

    4-3sectionstart

---
+++ 4-3section_kist/4-3section_kist/__3section_kistApp.swift
@@ -0,0 +1,17 @@
+//
+//  __3section_kistApp.swift
+//  4-3section_kist
+//
+//  Created by 市川マサル on 2021/04/29.
+//
+
+import SwiftUI
+
+@main
+struct __3section_kistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
