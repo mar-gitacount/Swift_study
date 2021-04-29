d4e5566c434c30d13e2a0c2df2f986515b1cedfb
市川マサル <>
2021-04-29 12:43:37 +0900

    リスト表示とナビゲーションリンク

---
+++ Navigation_Link/Navigation_Link/Navigation_LinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  Navigation_LinkApp.swift
+//  Navigation_Link
+//
+//  Created by 市川マサル on 2021/04/29.
+//
+
+import SwiftUI
+
+@main
+struct Navigation_LinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
