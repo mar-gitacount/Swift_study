8eafaaccf0c9dbda2bdd6048d939f0153fb3f2ea
市川マサル <>
2021-04-29 10:49:27 +0900

    画像効果を使う方法

---
+++ image_rotate/image_rotate/image_rotateApp.swift
@@ -0,0 +1,17 @@
+//
+//  image_rotateApp.swift
+//  image_rotate
+//
+//  Created by 市川マサル on 2021/04/29.
+//
+
+import SwiftUI
+
+@main
+struct image_rotateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
