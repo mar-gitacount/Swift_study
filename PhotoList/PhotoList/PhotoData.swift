f13a43c2f384c158650d2fe36d96964921679b55
市川マサル <>
2021-05-01 00:27:25 +0900

    no message

--- PhotoList/PhotoList/PhotoData.swift
+++ PhotoList/PhotoList/PhotoData.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+
+//写真データを配列に入れる
+var PhotoArray:[PhotoData] = makeData()
+//写真データを構造体で定義する
+struct PhotoData: Identifiable{
+    var id: Int
+    var imageName: String
+    var title: String
+}
+
+
+func makeData()->[PhotoData]{
+    //構造体PhotoData型の写真データが入った配列を作る。
+    var dataArray:[PhotoData] = []
+    ///写真データを追加していく。
+    dataArray.append(PhotoData(id:1,imageName: "Image_4287", title: "ロキ"))
+    dataArray.append(PhotoData(id:2,imageName: "Image_4347", title:"ヨシツネ"))
+    dataArray.append(PhotoData(id:3,imageName: "Image_4348",title: "ルシファー"))
+    dataArray.append(PhotoData(id:4,imageName: "Image_4349",title: "オオクニヌシ"))
+    //写真が入った配列を返す
+    return dataArray
+}
