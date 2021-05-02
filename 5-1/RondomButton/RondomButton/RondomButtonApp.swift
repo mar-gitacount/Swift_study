ed9f9c736c1f18970d0bd0fb3fe05ec5041579db
市川マサル <>
2021-05-02 15:29:51 +0900

    5-1開始

---
+++ 5-1/RondomButton/RondomButton/RondomButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  RondomButtonApp.swift
+//  RondomButton
+//
+//  Created by 市川マサル on 2021/05/02.
+//
+
+import SwiftUI
+
+@main
+struct RondomButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
