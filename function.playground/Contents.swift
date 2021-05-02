71dcc9979feb3bbe9a596ea982cf8484d56a7b79
市川マサル <>
2021-05-02 13:12:41 +0900

    section4-5 値を返す関数と返さない関数の実行ファイル

--- function.playground/Contents.swift
+++ function.playground/Contents.swift
@@ -1,3 +1,20 @@
 import UIKit
 
-var greeting = "Hello, playground"
+//デフォルトでは0を設定する関数を定義する
+
+func calc(adult:Int = 0 ,child:Int = 0) -> Int {
+    let money = adult * 1200 + child * 500
+    return money
+}
+//初期値はfalse
+var isPlay = false
+
+//値を返さない関数
+func play() -> Void {
+    isPlay = true
+}
+//関数を実行する
+play()
+
+//isplayがtrueになっている
+print(isPlay)
