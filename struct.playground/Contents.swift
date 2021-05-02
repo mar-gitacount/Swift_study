cc33202d0fcb8a402ae2d792abc210f7c978fbae
市川マサル <>
2021-05-02 11:41:45 +0900

    ユーザー定義関数の定義と実行をする

--- struct.playground/Contents.swift
+++ struct.playground/Contents.swift
@@ -1,21 +1,12 @@
 import UIKit
 
-struct Member {
-    let name:String
-    var level = 1
-    var age:Int
+
+//関数の定義をする
+func calc(adult:Int,child:Int) -> Int {
+    let money = adult * 1200 + child * 500
+    return money
 }
 
-//構造体を作る
-var member1 = Member(name:"鈴木",age:16)
-var member2 = Member(name:"佐藤",level:5,age:13)
-
-//構造体の値を利用する
-let text1 = "\(member1.name)さん \(member1.age)歳、レベルは \(member1.level)です"
-print(text1)
-
-//member2のレベルに1を足す
-member2.level += 1
-//レベル6が出力される
-print(member2)
-
+//関数を実行する
+let price = calc(adult:3,child:0)
+print(price)
