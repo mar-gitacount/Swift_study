680932665b1391337252f6cbb0e29248fe90ccf1
市川マサル <>
2021-05-02 16:01:41 +0900

    乱数をコンソールに表示する

--- 5-1/RondomButton/RondomButton/ContentView.swift
+++ 5-1/RondomButton/RondomButton/ContentView.swift
@@ -9,12 +9,23 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Button(action: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/{}/*@END_MENU_TOKEN@*/) {
-            /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Content@*/Text("Button")/*@END_MENU_TOKEN@*/
+        Button(action: {
+            //0から100までの乱数を代入
+            let num = Int.random(in: 0...100)
+            //Debug preview
+            print(num)
+        }){
+            //丸四角系のボタンを作成
+            Text("Random Button")
+                .font(.largeTitle)
+                .frame(width: 280, height: 60, alignment: .center)
+                .foregroundColor(Color.white)
+                .background(Color.blue)
+                .cornerRadius(15,antialiased: true)
+            
         }
-    }
+   }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
