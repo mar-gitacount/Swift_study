17ddacffc064ee5b49f21c211c641c340436ad17
市川マサル <>
2021-05-02 16:14:05 +0900

    5-1 乱数をユーザー側に表示するコード

--- 5-1/RondomButton/RondomButton/ContentView.swift
+++ 5-1/RondomButton/RondomButton/ContentView.swift
@@ -8,12 +8,15 @@
 import SwiftUI
 
 struct ContentView: View {
+    //構造体の自身が書き換える変数
+    @State var num:Int = 0
     var body: some View {
+        
         Button(action: {
             //0から100までの乱数を代入
-            let num = Int.random(in: 0...100)
+            num = Int.random(in: 0...100)
             //Debug preview
-            print(num)
+            //print(num)
         }){
             //丸四角系のボタンを作成
             Text("Random Button")
@@ -24,6 +27,10 @@ struct ContentView: View {
                 .cornerRadius(15,antialiased: true)
             
         }
+        //結果を表示するテキスト
+        Text("\(num)")
+            .font(.largeTitle)
+            .padding()
    }
 }
 struct ContentView_Previews: PreviewProvider {
