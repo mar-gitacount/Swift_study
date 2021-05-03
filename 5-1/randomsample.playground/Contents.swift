6045071fb9e8c460df7727e37a1ca1c7532299bf
市川マサル <>
2021-05-03 11:26:56 +0900

    配列からランダムに文字列を取り出したりするコード

--- 5-1/randomsample.playground/Contents.swift
+++ 5-1/randomsample.playground/Contents.swift
@@ -1,7 +1,16 @@
 import UIKit
 
-for _ in 1 ... 5 {
-    let value = Bool.random()
-    print(value)
-}
+//let colors = ["green","red","blue","orange"]
+//for _ in 1...5{
+//    //配列colorsから要素をランダムに取り出す
+//    let item = colors.randomElement()
+//    //強制的にアンラップする
+//    print(item!)
+//}
 
+//文字列"ABCDEFGHIJKLMNから5文字を選ぶ
+let letters = "ABCDEFGHIJKLMN"
+for _ in 1 ... 5 {
+    let item = letters.randomElement()
+    print(item!)
+}
