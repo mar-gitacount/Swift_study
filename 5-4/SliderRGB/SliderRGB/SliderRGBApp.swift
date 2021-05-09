a326b4ad746d91d46e793abd1607903233b5ff00
市川マサル <>
2021-05-09 20:11:19 +0900

    SliderのRGBとそれのTIPS

---
+++ 5-4/SliderRGB/SliderRGB/SliderRGBApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderRGBApp.swift
+//  SliderRGB
+//
+//  Created by 市川マサル on 2021/05/06.
+//
+
+import SwiftUI
+
+@main
+struct SliderRGBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
