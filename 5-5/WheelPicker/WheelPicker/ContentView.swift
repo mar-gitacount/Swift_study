8683bfa2fc05b09ef3c0346681b45f89098079ba
市川マサル <>
2021-05-09 22:06:47 +0900

    5-5PickerTips

--- 5-5/WheelPicker/WheelPicker/ContentView.swift
+++ 5-5/WheelPicker/WheelPicker/ContentView.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var selectedColor = 0
+    let colorViews = [Color.red,Color.green,Color.blue]
+    let colorNames = ["Red","Green","Blue"]
     var body: some View {
         VStack {
             Picker(selection: $selectedColor, label: Text("Color")) {
@@ -16,10 +18,16 @@ struct ContentView: View {
                 Text("Green").tag(1)
                 Text("Blue").tag(2)
             }
-        }
+            HStack {
+                colorViews[selectedColor]
+                    .frame(width:50,height:100)
+                Text("value: \(selectedColor)").frame(width:60)
+                Text("\(colorNames[selectedColor])").frame(width:70)
+            }
+       }
+        .padding()
     }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
