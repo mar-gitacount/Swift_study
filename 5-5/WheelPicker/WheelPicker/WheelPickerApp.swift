1523c0c7fc7d08a8aff0a0ee6c64e06c21f24d13
市川マサル <>
2021-05-09 21:23:40 +0900

    5-5序章Tips

---
+++ 5-5/WheelPicker/WheelPicker/WheelPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WheelPickerApp.swift
+//  WheelPicker
+//
+//  Created by 市川マサル on 2021/05/09.
+//
+
+import SwiftUI
+
+@main
+struct WheelPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
