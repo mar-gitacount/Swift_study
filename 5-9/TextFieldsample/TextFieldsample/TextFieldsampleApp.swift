2e9c19d331fae0da0d13501ed7a95b222b0d12b4
市川マサル <>
2021-05-24 00:29:16 +0900

    5-9　テキストフィールドについて

---
+++ 5-9/TextFieldsample/TextFieldsample/TextFieldsampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextFieldsampleApp.swift
+//  TextFieldsample
+//
+//  Created by 市川マサル on 2021/05/24.
+//
+
+import SwiftUI
+
+@main
+struct TextFieldsampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
