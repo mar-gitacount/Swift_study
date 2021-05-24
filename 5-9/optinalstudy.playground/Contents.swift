d995284d8cc3fc75e97dc7c2149e0d285ea57866
市川マサル <>
2021-05-25 00:32:15 +0900

    5-9まとめ

---
+++ 5-9/optinalstudy.playground/Contents.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+
+
+var nums:[Int] = [1,2,3]
+//lastNumには3が代入されるはずだがされない。
+let lastNum = nums.last
+let ans = lastNum * 2
---
+++ 5-9/optionalvaluetest.playground/Contents.swift
@@ -0,0 +1,6 @@
+iamport UIKit
+
+let nums:[Int] = []
+let lastNum = nums.last ?? 0
+let ans = lastNum * 2
+print(ans)
